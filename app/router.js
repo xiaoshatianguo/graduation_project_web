ecd9d2848a10ed51a38a0c76397b55e63cd220d7
songyeyi <songyeyi@talkmoney.cn>
2018-01-24 16:53:49 +0800

    <U>修改router文件命名规范

--- app/router.js
+++ app/router.js
@@ -10,17 +10,17 @@ module.exports = app => {
    * 前端路由跳转
    */
   /** 主要路由跳转 */
-  router.get('/', controller.Router.index);
-  router.get('/activity', controller.Router.activity);
-  router.get('/certified_architect', controller.Router.certifiedArchitect);
-  router.get('/personal_space', controller.Router.personalSpace);
+  router.get('/', controller.router.index);
+  router.get('/activity', controller.router.activity);
+  router.get('/certified_architect', controller.router.certifiedArchitect);
+  router.get('/personal_space', controller.router.personalSpace);
 
   /** 其他路由跳转 */
-  router.get('/activity_detail', controller.Router.activityDetail);
-  router.get('/join_activity', controller.Router.joinActivity);
-  router.get('/apply_activity', controller.Router.applyActivity);
-  router.get('/activity_manage', controller.Router.activityManage);
-  router.get('/apply_certified_architect', controller.Router.applyCertifiedArchitect);
-  router.get('/sort', controller.Router.sort);
-  router.get('/production_detail', controller.Router.productionDetail);
+  router.get('/activity_detail', controller.router.activityDetail);
+  router.get('/join_activity', controller.router.joinActivity);
+  router.get('/apply_activity', controller.router.applyActivity);
+  router.get('/activity_manage', controller.router.activityManage);
+  router.get('/apply_certified_architect', controller.router.applyCertifiedArchitect);
+  router.get('/sort', controller.router.sort);
+  router.get('/production_detail', controller.router.productionDetail);
 };
