8a820010f7aa3651241f32d9166ee75b63aef1fb
songyeyi <songyeyi@talkmoney.cn>
2018-05-03 16:23:38 +0800

    <U>修改评论表

--- app/init/sql/comments_info.sql
+++ app/init/sql/comments_info.sql
@@ -3,11 +3,15 @@ CREATE TABLE IF NOT EXISTS `comments_info` (
   `id` int(64) NOT NULL AUTO_INCREMENT,
   `user_id` VARCHAR(255) DEFAULT '' comment '评论或留言者',
   `object_id` VARCHAR(255) DEFAULT '' comment '接收评论或留言者',
-  `sort` INT(2) DEFAULT 0 comment '评论或留言分类 - 0 评论作品 - 1 评论活动 - 2 个人空间留言',
-  `comment_id` VARCHAR(255) DEFAULT '' comment '触发的id',
+  `production_id` VARCHAR(255) DEFAULT '' comment '评论的作品id',
+  `activity_id` VARCHAR(255) DEFAULT '' comment '评论的活动id',
+  `personal_id` VARCHAR(255) DEFAULT '' comment '留言的用户id',
   `content` VARCHAR(255) DEFAULT '' comment '评论或留言内容',
+  `reply_id` VARCHAR(255) DEFAULT '' comment '评论父id',
+  `star` VARCHAR(255) DEFAULT '' comment '评论被赞数量',
   `create_time` bigint comment '评论或留言发布时间',
-  `status` INT(2) DEFAULT 0 comment '是否被禁 - 0 无禁 - 1 禁',
+  `status` INT(2) DEFAULT 0 comment '是否被禁 - 0 否 - 1 禁',
+  `is_delete` INT(2) DEFAULT 0 comment '是否删除 - 0 否 - 1 删',
   `auditor` VARCHAR(255) DEFAULT '' comment '处理管理员编号',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
